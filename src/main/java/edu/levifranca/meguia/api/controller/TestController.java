c370d4478cd2d0dcd8be3ff15fb7b60aec5cdfa9
Levi Franca <levi@ciandt.com>
2016-10-09 21:30:31 -0300

    API WAR initial commit

---
+++ src/main/java/edu/levifranca/meguia/api/controller/TestController.java
@@ -0,0 +1,22 @@
+package edu.levifranca.meguia.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class TestController {
+
+    @RequestMapping(path="/", method = RequestMethod.GET)
+    public String root() {
+    	
+        return "Greetings from Me Guia API!";
+    }
+
+    @RequestMapping(path="/index", method = RequestMethod.GET)
+    public String index() {
+    	
+        return "INDEX";
+    }
+}
