0cb604a7dafda0261c4c172630fd34f293b04a91
Levi Franca <levi@ciandt.com>
2016-10-10 22:08:27 -0300

    Add Controller classes

---
+++ src/main/java/edu/metrocamp/meguia/api/controller/IdentificacaoBeaconController.java
@@ -0,0 +1,10 @@
+package edu.metrocamp.meguia.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/")
+public class IdentificacaoBeaconController {
+
+}
